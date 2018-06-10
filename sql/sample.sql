824b7d8435c94b668aec3baa171b4af30c55a216
atcvetkov <a.e.tsvetkov@gmail.com>
2018-06-10 18:34:01 +0800

    Add sql samples. Add ignored tmp folder. Disabled camml sample

---
+++ sql/sample.sql
@@ -0,0 +1,23 @@
+-- drop database test;
+create database test;
+
+use test;
+
+create table master(
+    id int(11),
+name varchar(50),
+primary key (id)
+);
+
+create table detail(
+id int(11),
+master_id int(11),
+value1 varchar(200),
+value2 varchar(200),
+primary key (id),
+FOREIGN KEY master_ref(id) references master(id)
+);
+
+drop table detail;
+drop table master;
+
