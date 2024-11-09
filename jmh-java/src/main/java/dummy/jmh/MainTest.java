d972f952a6d47a6f2ed7ce0356fe1282e56fb6c7
Aleksei Tsvetkov <a.e.tsvetkov@gmail.com>
2024-11-09 15:55:39 +0300

    Test for https://stackoverflow.com/questions/79170984/stream-through-a-portion-of-a-2d-array-in-java/79171050

--- jmh-java/src/main/java/dummy/jmh/MainTest.java
+++ jmh-java/src/main/java/dummy/jmh/MainTest.java
@@ -1,41 +1,86 @@
 package dummy.jmh;
 
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.IntStream;
 
-@Fork(value = 3, warmups = 1)
+@Fork(value = 1, warmups = 1)
 @BenchmarkMode(Mode.AverageTime)
 @State(Scope.Benchmark)
 public class MainTest {
+    private static final int FIRST_SIZE = 1000;
+    private static final int SECOND_SIZE = 100000;
     private int[][] arr;
 
     @Setup(Level.Trial)
     public void setUp() {
-        arr = IntStream.range(0, 1000)
-                .mapToObj(x -> new Random().ints(100000).toArray())
+        arr = IntStream.range(0, FIRST_SIZE)
+                .mapToObj(x -> new Random().ints(SECOND_SIZE).toArray())
                 .toArray(int[][]::new);
     }
 
     @Benchmark
-    public int loop() {
+    public void loop(Blackhole blackhole) {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 sum += arr[i][j];
             }
         }
-        return sum;
+        blackhole.consume(sum);
     }
 
     @Benchmark
-    public int stream() {
-        return Arrays.stream(arr)
+    public void singleLoop(Blackhole blackhole) {
+        int sum = 0;
+        for (int i = 0; i < FIRST_SIZE * SECOND_SIZE; i++) {
+            sum += arr[i / SECOND_SIZE][i % SECOND_SIZE];
+        }
+        blackhole.consume(sum);
+    }
+
+    @Benchmark
+    public void enhancedLoop(Blackhole blackhole) {
+        int sum = 0;
+        for (int[] ints : arr) {
+            for (int anInt : ints) {
+                sum += anInt;
+            }
+        }
+        blackhole.consume(sum);
+    }
+
+    @Benchmark
+    public void stream(Blackhole blackhole) {
+        int sum = Arrays.stream(arr)
                 .flatMapToInt(a -> Arrays.stream(a))
                 .sum();
+        blackhole.consume(sum);
+    }
+
+    @Benchmark
+    public void parallelStream(Blackhole blackhole) {
+        int sum = Arrays.stream(arr)
+                .parallel()
+                .flatMapToInt(a -> Arrays.stream(a))
+                .sum();
+        blackhole.consume(sum);
+    }
+
+
+    @Benchmark
+    public void mapMultiToInt(Blackhole blackhole) {
+        int sum = Arrays.stream(arr).mapMultiToInt((row, consumer) -> {
+            for (int i = 0; i < row.length; i++) {
+                consumer.accept(row[i]);
+            }
+        }).sum();
+
+        blackhole.consume(sum);
     }
 
     public static void main(String[] args) throws IOException {
