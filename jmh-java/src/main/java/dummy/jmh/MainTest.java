a22f04dfc54961ffc2a28b9f4db4f6c2511ab5fa
Aleksei Tsvetkov <a.e.tsvetkov@gmail.com>
2024-11-09 11:19:43 +0300

    Test for https://stackoverflow.com/questions/79170984/stream-through-a-portion-of-a-2d-array-in-java/79171050

--- jmh-java/src/main/java/dummy/jmh/MainTest.java
+++ jmh-java/src/main/java/dummy/jmh/MainTest.java
@@ -1,14 +1,41 @@
 package dummy.jmh;
 
-import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.*;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
 
+@Fork(value = 3, warmups = 1)
+@BenchmarkMode(Mode.AverageTime)
+@State(Scope.Benchmark)
 public class MainTest {
+    private int[][] arr;
+
+    @Setup(Level.Trial)
+    public void setUp() {
+        arr = IntStream.range(0, 1000)
+                .mapToObj(x -> new Random().ints(100000).toArray())
+                .toArray(int[][]::new);
+    }
 
     @Benchmark
-    public void wellHelloThere() {
-        // this method was intentionally left blank.
+    public int loop() {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                sum += arr[i][j];
+            }
+        }
+        return sum;
+    }
+
+    @Benchmark
+    public int stream() {
+        return Arrays.stream(arr)
+                .flatMapToInt(a -> Arrays.stream(a))
+                .sum();
     }
 
     public static void main(String[] args) throws IOException {
